3392c5a723a9512f0641a0092a7a272fa2a20fbf
Rainy <939029835@qq.com>
2026-01-12 16:59:36 +0800

    test blendmode

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 package;
 
-import flash.display.Bitmap;
-import flash.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
 import openfl.Assets;
 import openfl.display.Sprite;
 import openfl.Lib;
@@ -11,63 +11,21 @@ import openfl.geom.ColorTransform;
  * ...
  * @author Toby Davis
  */
-class Main extends Sprite 
-{
-
-	public function new() 
-	{
+class Main extends Sprite {
+	public function new() {
 		super();
-		
-		// Background color
-		var background:Sprite = new Sprite();
-		background.graphics.beginFill(0x282828);
-		background.graphics.drawRect(0, 0, 800, 480);
-		background.graphics.endFill();
-		
-		addChild(background);
-		
-		// Display container for bitmaps
-		var display:Sprite = new Sprite();
-		display.graphics.beginFill(0x7b7bc0);
-		display.graphics.drawRect(0, 0, 320, 240);
-		display.graphics.endFill();
-		
-		addChild(display);
-		
-		var image:BitmapData = Assets.getBitmapData("img/square2.png");
-		
-		// Bitmap with a add effect
-		var spriteAdd:Bitmap = new Bitmap(image);
-		spriteAdd.blendMode = "add"; 
-		
-		// Bitmap with a subtract effect
-		var spriteSub:Bitmap = new Bitmap(image);
-		spriteSub.blendMode = "subtract";
-		
-		// Bitmap with a invert effect
-		var spriteInvert:Bitmap = new Bitmap(image);
-		spriteInvert.blendMode = "normal";
-		spriteInvert.transform.colorTransform = new ColorTransform( -1, -1, -1, 1, 0, 0, 0, 0);
-		
-		// Set image positions
-		spriteAdd.x = 0;
-		spriteSub.x = 64;
-		spriteInvert.x = 128;
-		
-		// Add bitmaps to display container
-		display.addChild(spriteAdd);
-		display.addChild(spriteSub);
-		display.addChild(spriteInvert);
-		
-		// Bitmap data that will capture a screenshot of the display sprite
-		var captureData:BitmapData = new BitmapData(320, 240, false);
-		captureData.disposeImage();
-		captureData.draw(display);
-		
-		// Add captured image to the stage for comparison (shown on the right)
-		var captureSurface:Bitmap = new Bitmap(captureData);
-		captureSurface.x = 400;
-		addChild(captureSurface);
-	}
 
+		this.stage.color = 0xffffff;
+
+		var image0a:BitmapData = Assets.getBitmapData("img/blendMode-0a.jpg");
+		var image0b:BitmapData = Assets.getBitmapData("img/blendMode-0b.jpg");
+
+		var image1:Bitmap = new Bitmap(image0a);
+		addChild(image1);
+		var image2:Bitmap = new Bitmap(image0b);
+		addChild(image2);
+		image2.x = image1.width * 0.4;
+		image2.y = image1.height * 0.4;
+		image2.blendMode = SUBTRACT;
+	}
 }
